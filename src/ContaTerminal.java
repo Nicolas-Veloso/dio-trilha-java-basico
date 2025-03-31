583d9a00632fce70430298a021ddcf47c30955ff
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-03-30 22:50:58 -0300

    Projeto finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o número da agência: ");
+        int numeroAgencia = scan.nextInt();
+        System.out.println("Informe o nome da agência: ");
+        String nomeAgencia = scan.next();
+        System.out.println("Informe o nome do cliente: ");
+        String nomeCliente = scan.next();;
+        System.out.println("Informe o saldo da conta: ");
+        double saldo = scan.nextDouble();
+        System.out.println("Olá," +nomeCliente+ " obrigado por criar uma conta em nosso banco, sua agência é, " +nomeAgencia+ " conta " +numeroAgencia+ " e seu saldo " +saldo+ " já está disponível para saque");
     }
 }
